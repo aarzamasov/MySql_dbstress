6ce0c16f93d73500acbbca3ea0f5b7064e2582b7
Artyom Arzamasov <Sorok7@gmail.com>
2011-12-19 20:21:09 +0500

    1

--- run.sh
+++ run.sh
@@ -14,9 +14,10 @@ function killer()
 TESTS="READ_KEY_POINT READ_KEY_POINT_NO_DATA READ_KEY_POINT_LIMIT READ_KEY_POINT_NO_DATA_LIMIT UPDATE_POINT_PK UPDATE_RANGE_PK"
 for test in $TESTS;
 do
-        for threads in 1 4 16 64 128 256 384 512 600; 
+        #for threads in 1 4 16 64 128 256 384 512 600;
+        for threads in 1 4 16 64 128 256 384 512 600;
         do
-	       	php main.php  -d 360 -t $threads -o run -n df_enum -s 1000000 -e myisam -q $test
+	       	php main.php  -d 3 -t $threads -o run -n df_enum -s 1000000 -e myisam -q $test
                 killer
         done
 done
